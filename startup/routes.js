27749707e450e1d70a9cd9115b7f2e53a94cce7f
basitol <basitolaitan27@gmail.com>
2023-12-18 20:46:21 +0100

    update route for product routes

--- startup/routes.js
+++ startup/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const users = require('../routes/userRoutes');
+const products = require('../routes/productRoute');
 
 module.exports = function (app) {
   app.use('/api/users', users);
+  app.use('/api', products);
 };
