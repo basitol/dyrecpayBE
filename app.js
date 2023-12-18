5d01add8712f174b7604297e2c773b2895114fad
basitol <basitolaitan27@gmail.com>
2023-12-18 20:44:46 +0100

    update the root file

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ const app = express();
 
 // Use cors middleware for handling CORS (Cross-Origin Resource Sharing) requests
 app.use(cors());
+app.use(express.json({limit: '10mb'}));
 
 // Use built-in middleware for parsing JSON and URL-encoded data
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({limit: '10mb', extended: true}));
+
+// Include your routes and database initializations
+require('./startup/routes')(app);
+require('./startup/db')();
 
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Welcome to the Wicartit server');
 });
-
-// Include your routes and database initializations
-require('./startup/routes')(app);
-require('./startup/db')();
 
 // Start the server
 const PORT = process.env.PORT || 3000;
