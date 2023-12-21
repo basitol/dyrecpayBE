df9564afde27c92148ee7a66a18cc0275b837b6a
basitol <basitolaitan27@gmail.com>
2023-12-21 12:26:21 +0100

    build order routes

---
+++ routes/orderRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const orderController = require('../controllers/orderController'); // Adjust the path as necessary
+
+// Create a new order
+router.post('/orders', orderController.createOrder);
+
+// Get order by ID
+router.get('/orders/:orderId', orderController.getOrderById);
+
+// Update order status
+router.put('/orders/:orderId', orderController.updateOrderStatus);
+
+module.exports = router;
