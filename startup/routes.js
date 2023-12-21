3e18c5868e5c926531e036a897c3e330f1c93cd5
basitol <basitolaitan27@gmail.com>
2023-12-21 12:26:49 +0100

    update all routes file

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const users = require('../routes/userRoutes');
+const auth = require('../routes/authRoutes');
 const products = require('../routes/productRoute');
+const cart = require('../routes/cartRoutes');
+const order = require('../routes/orderRoutes');
 
 module.exports = function (app) {
   app.use('/api/users', users);
+  app.use('/api/auth', auth);
   app.use('/api', products);
+  app.use('/api', cart);
+  app.use('/api', order);
 };
