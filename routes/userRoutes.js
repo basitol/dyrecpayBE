67551809d8d7793d115ed93417a1f8fb397b654a
basitol <basitolaitan27@gmail.com>
2023-12-21 12:26:32 +0100

    build user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {loginUser, registerUser} = require('../controllers/userController');
+const {
+  loginUser,
+  registerUser,
+  deleteUser,
+  getUser,
+} = require('../controllers/userController');
 
 // Route for user login
 router.post('/login', loginUser);
 
 // Route for user registration
 router.post('/register', registerUser);
+
+// Route for getting user details
+router.get('/:id', getUser);
+
+// Route for deleting a user
+// This route is typically private and requires authentication
+router.delete('/:id', deleteUser);
 
 module.exports = router;
