f38e65a8896216640adfb0629fcea4e99be22046
basitol <basitolaitan27@gmail.com>
2024-01-07 03:34:55 +0100

    remove test

--- startup/db.js
+++ startup/db.js
@@ -5,11 +5,11 @@ module.exports = function () {
   mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
-      //   winston.info('Connected to MongoDB Test...');
-      console.log('Connected to MongoDB Test...');
+      //   winston.info('Connected to MongoDB...');
+      console.log('Connected to MongoDB...');
     })
     .catch(err => {
-      //   winston.error('Could not connect to MongoDB Test...', err);
-      console.log('Could not connect to MongoDB Test...', err);
+      //   winston.error('Could not connect to MongoDB...', err);
+      console.log('Could not connect to MongoDB...', err);
     });
 };
