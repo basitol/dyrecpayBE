e68e2dbf6430184b60f216e33c8373f9f2a12686
basitol <basitolaitan27@gmail.com>
2024-01-09 18:04:25 +0100

    update payment and delivery status in order route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -8,8 +8,11 @@ router.post('/orders', orderController.createOrder);
 // Get order by ID
 router.get('/orders/:orderId', orderController.getOrderById);
 
-// Update order status
-router.put('/orders/:orderId', orderController.updateOrderStatus);
+// Update order status to paid
+router.put('/orders/:orderId/paid', orderController.updateOrderToPaid); // New route for updating order to paid
+
+// Update delivery status (Admin only)
+router.put('/orders/:orderId/delivery', orderController.updateDeliveryStatus); // New route for admin to update delivery status
 
 router.get('/orders/user/:userId', orderController.getOrdersByUserId);
 
